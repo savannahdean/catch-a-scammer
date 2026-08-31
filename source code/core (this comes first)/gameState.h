fc8ba330892bf336e14d9c9608d6f85d2249bc89
Haley Crousser <haleycrousser99@gmail.com>
2026-08-31 14:08:25 -0500

    to-do comments in files to get a brief understanding of what to do first.

---
+++ source code/core (this comes first)/gameState.h
@@ -0,0 +1,23 @@
+//the state of the game
+
+//title screen
+//loading screen --> optional, shouldn't be priority
+//pause screen --> optional, shouldn't be priority
+//game screen
+//end game screen --> start new case or exit to title screen
+
+//step 1, lets work on title screen. this will help me get an understanding of raylib buttons.
+//we will have a start game button and exit button.
+//OPTIONAL FOR LATER: We can add a settings button, if we ever add sounds, maybe a fullscreen mode/resolution
+
+//class TitleScreen
+    //we might need an implementation file, but lets see how long it is. it should be incredibly long.
+    //It could have a hackerish looking background, maybe green blue and black colors.
+    //we should not spend too long on the design, but I want there to be a little so Savannah has code to look at.
+    //when start button is pressed, it should play the gameloop
+
+
+//class Game_Loop_START()
+    //starts the game loop...
+    //thinking of how to start this as of 8/31, but we may need to start with tables to get the data.
+    //However; we can go ahead and get a blank window going for the fron end.
